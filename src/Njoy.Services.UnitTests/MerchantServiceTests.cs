2b0213d5bd38e0860380c3a6168b53663dcce181
Mert Akcakaya <amert.akcakaya@gmail.com>
2019-05-05 17:52:37 +0300

    MerchantService and tests are developed

---
+++ src/Njoy.Services.UnitTests/MerchantServiceTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Njoy.Data;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Njoy.Services
+{
+    public sealed class MerchantServiceTests
+    {
+        [Fact]
+        public async void Search_Returns_Only_Merchants_With_Usernames_Containing_Search_Phrase()
+        {
+            const int userCount = 10;
+            var context = GetContext();
+            var searchKey = "user";
+            var usernames = new List<string>();
+            for (var i = 0; i < userCount; i++)
+            {
+                var halfReached = i >= userCount / 2;
+                var username = !halfReached ? $"test{searchKey}name{i}" : $"testname{i}";
+                if (!halfReached)
+                {
+                    usernames.Add(username);
+                }
+                var user = new AppUser
+                {
+                    UserName = username,
+                    NormalizedUserName = username.ToUpper()
+                };
+                context.Users.Add(user);
+
+                var merchant = new Merchant
+                {
+                    UserId = user.Id,
+                };
+                context.Set<Merchant>().Add(merchant);
+            };
+            context.SaveChanges();
+
+            var service = new MerchantService(context);
+            var result = await service.Search(searchKey);
+            Assert.Equal(userCount / 2, result.Count());
+            for (var i = 0; i < userCount / 2; i++)
+            {
+                Assert.Contains(result, m => m.User.UserName == usernames[i]);
+            }
+        }
+
+        private NjoyContext GetContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<NjoyContext>();
+            optionsBuilder.UseInMemoryDatabase(nameof(MerchantServiceTests));
+            return new NjoyContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ src/Njoy.Services/Merchant/MerchantService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nensure;
+using Njoy.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Njoy.Services
+{
+    public sealed class MerchantService : IMerchantService
+    {
+        private readonly NjoyContext _context;
+
+        public MerchantService(NjoyContext context)
+        {
+            Ensure.NotNull(context);
+            _context = context;
+        }
+
+        public async Task<Merchant> Get(int id)
+        {
+            return await _context.Set<Merchant>().FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Merchant>> Search(string username)
+        {
+            return await _context.Set<Merchant>()
+                .Where(m => m.User.NormalizedUserName.Contains(username.ToUpper()))
+                .ToArrayAsync();
+        }
+    }
+}
